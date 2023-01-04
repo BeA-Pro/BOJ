f427fb2eb2995ed25e6b68095101dad1af9459ca
YongHyunKing <99469068+YongHyunKing@users.noreply.github.com>
2023-01-04 21:32:02 +0900

    23/01/04

--- 14225.cpp
+++ 14225.cpp
@@ -23,10 +23,40 @@
 
 using namespace std;
 
-void input() {
+bool check[2000001];
+int N;
+vector<int> v;
+
+void cal(int idx,int value){
+    check[value]=true;
+
+    if(idx>=N) return;
+    check[v[idx]] = true;
+
+    //현재 경우를 포함 안하는 경우
+    cal(idx+1,value);
+
+    //현재 경우를 포함하는 경우
+    cal(idx+1,value+v[idx]);
 }
 
+void input() {
+    cin>>N;
+    v.resize(N);
+    for(int i=0;i<N;i++) cin>>v[i];
+}
+
+
 void solution() {
+    cal(0,0);
+
+    for(int i=0;i<=2000000;i++){
+        if(!check[i]){
+            cout<<i;
+            return;
+        }
+    }
+
 }
 
 int main()
