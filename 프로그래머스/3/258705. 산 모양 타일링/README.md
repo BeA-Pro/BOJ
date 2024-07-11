4a65d17a7ce70350808e76827e18a1bb39972009
BeAPro <99469068+BeA-Pro@users.noreply.github.com>
2024-07-11 16:25:14 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.09 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/README.md
+++ 프로그래머스/3/258705. 산 모양 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 산 모양 타일링 - 258705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 7.31 ms
+메모리: 81.5 MB, 시간: 7.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 11일 16:16:12
+2024년 07월 11일 16:25:11
 
 ### 문제 설명
 
--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -13,16 +13,14 @@ class Solution {
         
         for(int i=1;i<n;i++){
             if(tops[i] == 1){ // top이 존재 할 때
-                dp[i][0] = ((dp[i-1][0] * 1) + (dp[i-1][1] * 1))%mod;
-                dp[i][1] = ((dp[i-1][0] * 2) + (dp[i-1][1] * 3))%mod;
+                dp[i][0] = (dp[i-1][0] + dp[i-1][1] )%mod;
+                dp[i][1] = (dp[i-1][0] * 2 + dp[i-1][1] * 3)%mod;
             }else{
-                dp[i][0] = ((dp[i-1][0] * 1) + (dp[i-1][1] * 1))%mod;
-                dp[i][1] = ((dp[i-1][0] * 1) + (dp[i-1][1] * 2))%mod;
+                dp[i][0] = (dp[i-1][0] + dp[i-1][1])%mod;
+                dp[i][1] = (dp[i-1][0] + dp[i-1][1] * 2)%mod;
             }
         }
-        // for(int[] a : dp){
-        //     System.out.println(a[0] + " "+ a[1]);
-        // }
+
         return (dp[n-1][0] + dp[n-1][1])%mod;
     }
 }
